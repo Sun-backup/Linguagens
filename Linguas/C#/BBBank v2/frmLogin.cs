49d9d642560ad9909169b042b31ac45b6aeeebda
Pudimzin <pudimzinho100@gmail.com>
2022-04-03 16:29:14 -0300

    Banco 2.0

---
+++ Linguas/C#/BBBank v2/frmLogin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BBBank_v2
+{
+    public partial class frmLogin : Form
+    {
+
+        public string Numero { get; private set; }
+        public string Senha { get; private set; }
+
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogar_Click(object sender, EventArgs e)
+        {
+            this.Numero = txtNumeroConta.Text;
+            this.Senha = txtSenha.Text;
+            this.Close();
+        }
+
+        private void txtSenha_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNumeroConta_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
